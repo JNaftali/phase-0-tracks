7785fa2e36176e23abd53d0defcefd885b655980
JNaftali <marantzj@gmail.com>
2016-06-14 17:14:36 -0400

    4.4 release 3

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,47 +1,54 @@
-puts "Greetings, new employee! Welcome to Werewolves Inc! What is your name?"
-name = gets.chomp
+puts "How many new employees will be processed?"
+counter = gets.chomp.to_i
 
-puts "How old are you?"
-age = gets.chomp.to_i
+while counter > 0
 
-puts "What year were you born?"
-birthyear = gets.chomp.to_i
+  puts "Greetings, new employee! Welcome to Werewolves Inc! What is your name?"
+  name = gets.chomp
 
-puts "Our company cafeteria serves garlic bread. Should we order some for you?"
-garlic = gets.chomp
+  puts "How old are you?"
+  age = gets.chomp.to_i
 
-if garlic.include? "yes"
-  garlic = true
-else
-  garlic = false
+  puts "What year were you born?"
+  birthyear = gets.chomp.to_i
+
+  puts "Our company cafeteria serves garlic bread. Should we order some for you?"
+  garlic = gets.chomp
+
+  if garlic.include? "yes"
+    garlic = true
+  else
+    garlic = false
+  end
+
+  puts "Would you like to enroll in the company’s health insurance?"
+  insurance = gets.chomp
+
+  if insurance.include? "yes"
+    insurance = true
+  else
+    insurance = false
+  end
+
+  result = "Results inconclusive."
+
+  if age === Time.now.year - birthyear && (garlic || insurance)
+    result = "Probably not a vampire."
+  end
+
+  if age != Time.now.year - birthyear && (!garlic || !insurance)
+    result = "Probably a vampire."
+  end
+
+  if !(age === Time.now.year - birthyear || garlic || insurance)
+    result = "Almost certainly a vampire."
+  end
+
+  case name
+  when "Drake Cula", "Tu Fang"
+    result = "Definitely a vampire."
+  end
+
+  puts result
+  counter = counter - 1
 end
-
-puts "Would you like to enroll in the company’s health insurance?"
-insurance = gets.chomp
-
-if insurance.include? "yes"
-  insurance = true
-else
-  insurance = false
-end
-
-result = "Results inconclusive."
-
-if age === Time.now.year - birthyear && (garlic || insurance)
-  result = "Probably not a vampire."
-end
-
-if age != Time.now.year - birthyear && (!garlic || !insurance)
-  result = "Probably a vampire."
-end
-
-if !(age === Time.now.year - birthyear || garlic || insurance)
-  result = "Almost certainly a vampire."
-end
-
-case name
-when "Drake Cula", "Tu Fang"
-  result = "Definitely a vampire."
-end
-
-puts result
