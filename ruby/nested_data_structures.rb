a8ed61d6d9ac40863cfb026d784bc1f854e4f285
JNaftali <marantzj@gmail.com>
2016-06-20 16:39:04 -0400

    Added nested data structure.

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,32 @@
+old_lady = {
+  status: 'Alive and well, of course',
+  horse: true,
+  cow: {
+    status: 'I don\'t know how',
+    organs: [ 'liver', 'kidney', 'heart', 'stomach', 'cud' ],
+    goat: {
+      status: 'Opened her throat and down went the goat!',
+      organs: [ 'liver', 'kidney', 'heart', 'stomach' ],
+      dog: {
+        status: 'What a hog',
+        organs: [ 'liver', 'kidney', 'heart', 'stomach' ],
+        cat: {
+          status: 'Imagine that',
+          organs: [ 'liver', 'kidney', 'heart', 'stomach', 'hairball' ],
+          bird: {
+            status: 'How absurd',
+            organs: [ 'liver', 'kidney', 'heart', 'stomach', 'pebble' ],
+            spider: {
+              status: 'Wriggled and jiggled and tickled inside',
+              fly: {
+                status: 'I don\t know why'
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+p old_lady
