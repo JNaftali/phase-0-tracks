32f562d89fc81716c38770538dac540a037e714c
JNaftali <marantzj@gmail.com>
2016-06-20 16:02:15 -0400

    Made some improvements to vampires.rb, just to see how short I could make it

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,7 +1,7 @@
 puts "How many new employees will be processed?"
 counter = gets.chomp.to_i
 
-while counter > 0
+until counter == 0
 
   puts "Greetings, new employee! Welcome to Werewolves Inc! What is your name?"
   name = gets.chomp
@@ -12,23 +12,13 @@ while counter > 0
   puts "What year were you born?"
   birthyear = gets.chomp.to_i
 
-  puts "Our company cafeteria serves garlic bread. Should we order some for you?"
-  garlic = gets.chomp
+  age = age == Time.now.year - birthyear
 
-  if garlic.include? "yes"
-    garlic = true
-  else
-    garlic = false
-  end
+  puts "Our company cafeteria serves garlic bread. Should we order some for you?"
+  garlic = gets.chomp.include? "yes"
 
   puts "Would you like to enroll in the company’s health insurance?"
-  insurance = gets.chomp
-
-  if insurance.include? "yes"
-    insurance = true
-  else
-    insurance = false
-  end
+  insurance = gets.chomp.include? "yes"
 
   allergy = "temp"
   while allergy != "done"
@@ -46,15 +36,15 @@ while counter > 0
 
   result = "Results inconclusive."
 
-  if age === Time.now.year - birthyear && (garlic || insurance)
+  if age && ( garlic || insurance)
     result = "Probably not a vampire."
   end
 
-  if age != Time.now.year - birthyear && (!garlic || !insurance)
+  if !age && ( !garlic || !insurance)
     result = "Probably a vampire."
   end
 
-  if !(age === Time.now.year - birthyear || garlic || insurance)
+  if !( age || garlic || insurance)
     result = "Almost certainly a vampire."
   end
 
@@ -64,7 +54,7 @@ while counter > 0
   end
 
   puts result
-  counter = counter - 1
+  counter -= 1
 end
 
 puts "Actually, never mind! What do these questions have to do with anything? Let's all be friends."
