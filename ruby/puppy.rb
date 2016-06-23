bc0297be6a6f80e17de3bc1375f879cfddbbffe8
JNaftali <marantzj@gmail.com>
2016-06-23 00:17:43 -0400

    6.1

---
+++ ruby/puppy.rb
@@ -0,0 +1,14 @@
+#SPECIES ------------------------
+#Puppy
+#
+#CHARACTERISTICS ----------------
+#leg count: 4
+#Friendly: yes
+#Eye count: 2
+#Name: varies
+#Color: varies
+#
+#BEHAVIOR -----------------------
+#Jump
+#Wag
+#Piddle
