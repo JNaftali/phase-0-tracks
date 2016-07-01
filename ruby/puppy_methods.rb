03acb1f4a014d2a3ba7101a8cfb07e80bf571b60
JNaftali <marantzj@gmail.com>
2016-06-30 21:14:56 -0400

    6.2 release 0

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -5,5 +5,32 @@ class Puppy
     toy
   end
 
+  def woof ( times )
+    p "woof " * times
+  end
+
+  def roll_over
+    p "*rolls over*"
+  end
+
+  def dog_years ( h_years )
+    if h_years <= 2
+      p h_years * 10.5
+    else
+      p 21 + ( ( h_years - 2 ) * 4 )
+    end
+  end
+
+  def chase_tail
+    p "*spins around in circles*"
+  end
 end
 
+spot = Puppy.new
+#spot.fetch("ball")
+
+#spot.woof(5)
+
+#spot.dog_years(3)
+
+#spot.chase_tail
