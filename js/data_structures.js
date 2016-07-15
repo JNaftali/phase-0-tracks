0f9a4f4720c91f4935e04f64d436158cda063e1a
JNaftali <marantzj@gmail.com>
2016-07-15 17:37:39 -0400

    Finish release 0

---
+++ js/data_structures.js
@@ -0,0 +1,6 @@
+var colors = [ "red", "blue", "yellow", "green" ];
+var names = [ "ed", "dallas", "star", "buck" ];
+colors.push("purple")
+names.unshift("prancer")
+console.log(colors)
+console.log(names)
