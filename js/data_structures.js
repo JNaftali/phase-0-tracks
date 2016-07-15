f684a2e7e1455d5cf33cab7f219dff2dcdad0b53
JNaftali <marantzj@gmail.com>
2016-07-15 18:05:48 -0400

    Finish release 1

--- js/data_structures.js
+++ js/data_structures.js
@@ -1,6 +1,19 @@
 var colors = [ "red", "blue", "yellow", "green" ];
 var names = [ "ed", "dallas", "star", "buck" ];
-colors.push("purple")
-names.unshift("prancer")
-console.log(colors)
-console.log(names)
+colors.push("purple");
+names.unshift("prancer");
+console.log(colors);
+console.log(names);
+
+horses = {};
+//{
+//  ed: red
+//  dallas: blue
+//  {name: ed, color:blue}
+//}
+
+for (var i in colors) {
+  horses[colors[i]] = names[i];
+}
+
+console.log(horses)
